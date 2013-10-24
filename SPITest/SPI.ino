2afc4fce411bb055de3a67a36817ec594e4d10b0
Interactive Matter <info@interactive-matter.org>
2013-10-24 15:55:27 +0200

    it seems that we can write via SPI! yay!

---
+++ SPITest/SPI.ino
@@ -0,0 +1,31 @@
+
+void send43x(unsigned char tmc43x_register, unsigned long datagram) {
+  unsigned long i_datagram;
+
+  //select the TMC driver
+  digitalWrite(cs_squirrel,LOW);
+
+
+#ifdef DEBUG
+  Serial.print("Sending ");
+  Serial.println(datagram,HEX);
+#endif
+
+  //the first value is irgnored
+  SPI.transfer(tmc43x_register);
+  //write/read the values
+  i_datagram = SPI.transfer((datagram >> 16) & 0xff);
+  i_datagram <<= 8;
+  i_datagram |= SPI.transfer((datagram >>  8) & 0xff);
+  i_datagram <<= 8;
+  i_datagram |= SPI.transfer((datagram) & 0xff);
+  i_datagram >>= 4;
+
+#ifdef DEBUG
+  Serial.print("Received ");
+  Serial.println(i_datagram,HEX);
+#endif
+  //deselect the TMC chip
+  digitalWrite(cs_squirrel,HIGH); 
+}
+
--- SPITest/SPITest.ino
+++ SPITest/SPITest.ino
@@ -1,11 +1,25 @@
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
+#include <SPI.h>
+#define DEBUG
 
+int cs_squirrel = 7;
+
+void setup() {
+  //initialize SPI
+  SPI.begin();
+  pinMode(cs_squirrel,OUTPUT);
+  digitalWrite(cs_squirrel,HIGH);
+  //initialize the serial port for debugging
+  Serial.begin(9600);
 }
+
+unsigned long tmc43xx_write;
+unsigned long tmc43xx_read;
 
 void loop() {
   // put your main code here, to run repeatedly: 
-  Serial.println("yay");
-  delay(100);
+  send43x(0x21,0);
+  Serial.print("x actual:");
+  send43x(0x21,0);
+  Serial.println();
+  delay(1000);
 }
