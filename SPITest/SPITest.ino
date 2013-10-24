b48fb98e031e454e504c1ad7f4837cf47c122f20
Interactive Matter <info@interactive-matter.org>
2013-10-24 16:41:26 +0200

    trying to configure the TMC260

--- SPITest/SPITest.ino
+++ SPITest/SPITest.ino
@@ -1,6 +1,17 @@
 #include <SPI.h>
 #define DEBUG
 
+//motor config
+unsigned char steps_per_revolution = 200;
+
+//register
+#define SPIOUT_CONF 0x04
+#define STEP_CONF 0x0A
+
+//values
+#define TMC_26X_CONFIG 0xA
+
+
 int cs_squirrel = 7;
 
 void setup() {
@@ -10,6 +21,12 @@ void setup() {
   digitalWrite(cs_squirrel,HIGH);
   //initialize the serial port for debugging
   Serial.begin(9600);
+  //configure the TMC26x
+  send43x(SPIOUT_CONF,TMC_26X_CONFIG);
+  //configure the motor type
+  unsigned long motorconfig = 0xff; //we want closed loop operation
+  motorconfig |= steps_per_revolution<<4;
+  send43x(STEP_CONF,motorconfig);
 }
 
 unsigned long tmc43xx_write;
