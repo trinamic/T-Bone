45aa39038103fb9fc5cfc91acad1a7a4879a26a2
Interactive Matter <info@interactive-matter.org>
2013-11-05 11:48:57 +0100

    ok, this isthe final version of how to use shaddow reigsters
    _BV(5) in start config did the trick …

--- GearingTest/GearingTest.ino
+++ GearingTest/GearingTest.ino
@@ -11,7 +11,7 @@
 //config
 unsigned char steps_per_revolution = 200;
 unsigned int current_in_ma = 500;
-long vmax = 10000000ul;
+long vmax = 100000ul;
 long bow = 1000000;
 long end_bow = bow;
 long amax = vmax/100;
@@ -30,31 +30,32 @@ long dmax = amax;
 #define GEAR_RATIO_REGISTER 0x12
 #define START_DELAY_REGISTER 0x13
 #define RAMP_MODE_REGISTER 0x20
-#define SH_RAMP_MODE_REGISTER 0x40
 #define X_ACTUAL_REGISTER 0x21
 #define V_MAX_REGISTER 0x24
-#define SH_V_MAX_REGISTER 0x41
+#define V_START_REGISTER 0x25
+#define V_STOP_REGISTER 0x26
 #define A_MAX_REGISTER 0x28
-#define SH_A_MAX_REGISTER 0x44
 #define D_MAX_REGISTER 0x29
-#define SH_D_MAX_REGISTER 0x46
 #define BOW_1_REGISTER 0x2d
-#define SH_BOW_1_REGISTER 0x49
 #define BOW_2_REGISTER 0x2e
-#define SH_BOW_2_REGISTER 0x4a
 #define BOW_3_REGISTER 0x2f
-#define SH_BOW_3_REGISTER 0x4b
 #define BOW_4_REGISTER 0x30
-#define SH_BOW_4_REGISTER 0x4c
 #define CLK_FREQ_REGISTER 0x31
 #define X_TARGET_REGISTER 0x37
 #define X_TARGET_PIPE_0_REGSISTER 0x38
+#define SH_RAMP_MODE_REGISTER 0x40
+#define SH_V_MAX_REGISTER 0x41
+#define SH_V_START_REGISTER 0x42
+#define SH_V_STOP_REGISTER 0x43
+#define SH_VBREAK_REGISTER 0x44
+#define SH_A_MAX_REGISTER 0x45
+#define SH_D_MAX_REGISTER 0x46
+#define SH_BOW_1_REGISTER 0x49
+#define SH_BOW_2_REGISTER 0x4a
+#define SH_BOW_3_REGISTER 0x4b
+#define SH_BOW_4_REGISTER 0x4c
 #define COVER_LOW_REGISTER 0x6c
 #define COVER_HIGH_REGISTER 0x6d
-#define V_START_REGISTER 0x25
-#define SH_V_START_REGISTER 0x42
-#define V_STOP_REGISTER 0x26
-#define SH_V_STOP_REGISTER 0x43
 
 //values
 #define TMC_26X_CONFIG_SPI 0x8440000a //SPI-Out: block/low/high_time=8/4/4 Takte; CoverLength=autom; TMC26x
@@ -98,7 +99,6 @@ void setup() {
   pinMode(start_signal_pin,INPUT);
   digitalWrite(start_signal_pin,LOW);
   attachInterrupt(target_reached_interrupt_a,interrupt_a_handler,RISING);
-  attachInterrupt(4,start_handler,RISING);
   //initialize the serial port for debugging
   Serial.begin(9600);
   //initialize SPI
@@ -186,33 +186,72 @@ void loop() {
     prev_target = target;
     target = next_target;
     next_target= random(100000ul);
-    
+
     long prev_dir = dir;
-    
+
     dir = target-prev_target;
     dir /= abs(dir);
-    
+
     long next_dir = next_target - target;
     next_dir /= abs(next_dir);
-    
+
     prev_v = this_v;
     this_v = next_v;
-    next_v = vmax+random(10)*vmax;
+    next_v = random(100)*vmax;
     float gear_ratio = (float)random(201)/100.0;
-    
+
     long v_start;
     if (prev_dir == dir) {
       v_start = prev_v;
-    } else {
+    } 
+    else {
       v_start = 0;
     }
     long v_end;
     if (dir == next_dir) {
       v_end = this_v;
-    } else {
+    } 
+    else {
       v_end = 0;
     }
+
+    unsigned long digital_ratio = 0;//FIXED_8_24_MAKE(gear_ratio);
+    unsigned long v_max_fpm = FIXED_24_8_MAKE(this_v);
+    unsigned long v_start_fpm = FIXED_24_8_MAKE(v_start);
+    unsigned long v_stop_fpm = FIXED_24_8_MAKE(v_end); 
     
+    if (isMoving) {
+      toMove=false;
+      write43x(squirrel_a, X_TARGET_REGISTER,target);
+      write43x(squirrel_a, SH_V_MAX_REGISTER, v_max_fpm); //set the velocity - TODO recalculate float numbers
+      write43x(squirrel_a, SH_V_START_REGISTER, v_start_fpm);
+      write43x(squirrel_a, SH_V_STOP_REGISTER, v_stop_fpm);
+ 
+      write43x(squirrel_b, GEAR_RATIO_REGISTER,digital_ratio);
+    } 
+    else {
+      isMoving=true;
+      toMove=true;
+      write43x(squirrel_a, V_STOP_REGISTER,v_stop_fpm); //set the velocity - TODO recalculate float numbers
+      write43x(squirrel_a, V_MAX_REGISTER,v_max_fpm); //set the velocity - TODO recalculate float numbers
+      write43x(squirrel_a, X_TARGET_REGISTER,target);
+      write43x(squirrel_a, START_CONFIG_REGISTER,
+      _BV(4) | //use shaddow motion profiles
+      _BV(5) | //target reached triggers start event
+      _BV(6) | //target reached triggers start event
+      _BV(10) |//immediate start
+      _BV(11) | // the shaddow registers cycle
+      _BV(0)); //from now on listen to your own start signal
+
+      write43x(squirrel_b, GEAR_RATIO_REGISTER,digital_ratio);
+      write43x(squirrel_b, START_CONFIG_REGISTER,
+      _BV(3) | //gear ration requires start
+      _BV(5) | //external start is an event
+      _BV(10) | //we start w/o timer 
+      _BV(0));  //and due to a bug we NEED an internal start signal 
+   attachInterrupt(4,start_handler,RISING);
+   }
+
     Serial.print("Move to ");
     Serial.print(target);
     Serial.print(" with ");
@@ -229,30 +268,8 @@ void loop() {
     Serial.println(v_end);
     Serial.print("Gear ration: ");
     Serial.println(gear_ratio);
-    unsigned long digital_ratio = FIXED_8_24_MAKE(gear_ratio);
-    Serial.println();
-    if (isMoving) {
-      toMove=false;
-      unsigned long v_max_fpm = FIXED_24_8_MAKE(this_v);
-      write43x(squirrel_a, V_MAX_REGISTER, v_max_fpm); //set the velocity - TODO recalculate float numbers
-      write43x(squirrel_a, X_TARGET_REGISTER,target);
-      write43x(squirrel_b, GEAR_RATIO_REGISTER,digital_ratio);
-    } 
-    else {
-      isMoving=true;
-      write43x(squirrel_a, V_MAX_REGISTER,this_v << 8); //set the velocity - TODO recalculate float numbers
-      write43x(squirrel_a, X_TARGET_REGISTER,target);
-      write43x(squirrel_a, START_CONFIG_REGISTER,_BV(10) | _BV(6) | _BV(0)); //from now on listen to your own start signal
+    Serial.println(); 
 
-      write43x(squirrel_b, GEAR_RATIO_REGISTER,digital_ratio);
-      write43x(squirrel_b, START_CONFIG_REGISTER,
-      _BV(3) | //gear ration requires start
-      _BV(5) | //external start is an event
-      _BV(10) | //we start w/o timer 
-      _BV(4) | //use shaddow motion profiles
-      _BV(11) | // the shaddow registers cycle
-      _BV(0));  //and due to a bug we NEED an internal start signal 
-    }
 
   }
   if (checkMetro.check()) {
@@ -281,6 +298,11 @@ void interrupt_a_handler() {
 void start_handler() {
   //Serial.println("start");
 }
+
+
+
+
+
 
 
 
