addbf713a8994bbd5d99f35a5a452626b597a8ee
Interactive Matter <info@interactive-matter.org>
2013-11-12 11:28:39 +0100

    ok let's see if we can achieve more with a bit more elaborate decoding

--- gcode-printer/LowLevelInterface/Machine.py
+++ gcode-printer/LowLevelInterface/Machine.py
@@ -26,10 +26,12 @@ class Machine():
         if serialport is None:
             serialport = _default_serial_port
         self.serialport = serialport
+        self.remaining_buffer = ""
 
 
     def connect(self):
         self.machineSerial = serial.Serial(self.serialport, 115200, timeout=_default_timeout)
+        self.remaining_buffer = ""
         command = self._read_next_command()
         if not command or not command.return_code is -128:
             raise MachineError("Machine does not seem to be ready")
@@ -43,16 +45,20 @@ class Machine():
         return command
 
     def _doRead(self):
-        buff = ""
+        buff = self.remaining_buffer
         tic = time.time()
-        buff += self.machineSerial.read(128)
+        buff += self.machineSerial.read()
 
         # you can use if not ('\n' in buff) too if you don't like re
         while ((time.time() - tic) < _default_timeout) and (not self._commandEndMatcher.search(buff)):
-            print >> sys.stdout, (time.clock() - tic) * 60
-            buff += self.machineSerial.read(128)
+            buff += self.machineSerial.read()
 
-        return buff
+        if self._commandEndMatcher.search(buff):
+            split_result = buff.split(';', 1)
+            self.remaining_buffer = split_result[1]
+            return split_result[0]
+        else:
+            return ''
 
 
 class MachineCommand():
