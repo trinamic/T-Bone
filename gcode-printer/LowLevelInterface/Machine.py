ebcd19b6f8083cd01bcb0703942993e3ba164d59
root <root@beaglebone.(none)>
2013-11-12 10:00:12 +0100

    what is so strange with the clock??

--- gcode-printer/LowLevelInterface/Machine.py
+++ gcode-printer/LowLevelInterface/Machine.py
@@ -31,10 +31,11 @@ class Machine():
     def doRead(self):
         buff = ""
         tic = time.clock()
-        print >> sys.stdout, tic
+	print >> sys.stdout, "read it"
         buff += self.machineSerial.read(128)
         # you can use if not ('\n' in buff) too if you don't like re
         while ((time.clock() - tic) < _default_timeout) and (not self._commandEndMatcher.search(buff)):
+            print >> sys.stdout, (time.clock() - tic)*60
             buff += self.machineSerial.read(128)
 
         return buff
