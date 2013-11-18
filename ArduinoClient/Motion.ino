7a3155d8baeea019bff35ca1c6c3fca22e05fc6e
Interactive Matter <info@interactive-matter.org>
2013-11-18 16:06:06 +0100

    ok, let's start slowy … 
    … just moving the motors according to the queue

--- ArduinoClient/Motion.ino
+++ ArduinoClient/Motion.ino
@@ -25,7 +25,34 @@ void checkMotion() {
         gearingscount++;
       } 
       while (gearings[gearingscount].type == gearmotor);
-      //TODO execute the movement
+
+
+      byte geared_motors=0;
+      for (char i=0;i<gearingscount;i++) {
+        char geared_motor = gearings[i].motor;
+        //all motors mentioned here are configured
+        float gear_ratio = gearings[i].data.follow.gearing;
+        write43x(motors[geared_motor].cs_pin, GEAR_RATIO_REGISTER,FIXED_8_24_MAKE(gear_ratio));
+        geared_motor |= _BV(i);
+      }
+      for (char i; i<nr_of_motors;i++) {
+        write43x(motors[i].cs_pin, GENERAL_CONFIG_REGISTER, _BV(0) | _BV(1) | _BV(6)); //direct values and no clock
+        write43x(motors[i].cs_pin, START_CONFIG_REGISTER,
+        _BV(10)//immediate start
+        );  //and due to a bug we NEED an internal start signal 
+        if (geared_motors && _BV(i) == 0) {
+          //unconfigure non geared motors
+        write43x(motors[i].cs_pin, GEAR_RATIO_REGISTER,FIXED_8_24_MAKE(0));
+        }
+      }
+      //finally configure the running motor
+      char moved_motor = move.motor;
+      write43x(motors[moved_motor].cs_pin, START_CONFIG_REGISTER,
+      _BV(10) //immediate start
+      ); //from now on listen to your own start signal
+      write43x(motors[moved_motor].cs_pin, GENERAL_CONFIG_REGISTER, _BV(0) | _BV(1)); //we use direct values
+      write43x(motors[moved_motor].cs_pin, X_TARGET_REGISTER,move.data.move.target);
+      //ok we know that we are running
       is_running = true;
     } 
     else {
@@ -33,6 +60,12 @@ void checkMotion() {
     }
   }
 }
+
+
+void target_reached_handler() {
+  is_running=false;
+}
+
 
 
 
