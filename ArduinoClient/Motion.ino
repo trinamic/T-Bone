e388b0d7653691e202c1f135d7d86c5abefa8079
Interactive Matter <info@interactive-matter.org>
2013-11-21 10:45:50 +0100

    ok, it seems to be easier if we configure the start signal in the motion routine
    who knows which other start signals we might need …

--- ArduinoClient/Motion.ino
+++ ArduinoClient/Motion.ino
@@ -15,6 +15,7 @@ void stopMotion() {
 void checkMotion() {
   if (in_motion && !next_move_prepared) {
     if (moveQueue.count()>0) {
+      byte moving_motors=0;
       //analysze the movement (nad take a look at the next
       movement move = moveQueue.pop();
       movement followers[MAX_FOLLOWING_MOTORS];
@@ -29,8 +30,8 @@ void checkMotion() {
       Serial.print(move.data.move.vmax);
 #endif
       moveMotor(move.motor, move.data.move.target,move.data.move.vmax, 1, prepare_shaddow_registers);
+      moving_motors |= _BV(move.motor);
 
-      byte following_motors=0;
       movement follower;
       do {
         follower = moveQueue.peek();
@@ -43,15 +44,78 @@ void checkMotion() {
           Serial.print(follower.data.follow.factor,DEC);
 #endif
           moveMotor(follower.motor, follower.data.follow.target,move.data.move.vmax, follower.data.follow.factor, prepare_shaddow_registers);
-          following_motors |= _BV(follower.motor);
+          moving_motors |= _BV(follower.motor);
         }
       } 
       while (follower.type == followmotor);
 
       for (char i; i<nr_of_motors;i++) {
-        if (following_motors && _BV(i) == 0) {
+        //configure all non moving motors to stop
+        if (moving_motors && _BV(i) == 0) {
           moveMotor(i, 0,0, 0, prepare_shaddow_registers);
         }
+
+        /*
+  Master: (Startsignal generieren)
+         	bit4 = 1 (shadow on)
+         	bit9 je nach Bedarf und in Abstimmung mit dem Slave
+         	bit12= 1 (pipeline on)
+         
+         	rest = 0 (wenn kein zyklische und/oder verzögerte Shadowregisterübernahme gewünscht)
+         
+         Slave: (auf externes Startsignal reagieren)
+         	bit0 = 1
+         	bit5 = 1
+         	bit4 = 1 (shadow on)
+         	bit9 je nach Bedarf und in Abstimmung mit dem Master
+         	bit12= 1 (pipeline on)
+         
+         	rest = 0 (wenn kein zyklische und/oder verzögerte Shadowregisterübernahme gewünscht)
+         
+         Mindestens eines der ersten 5Bit muss eingeschaltet sein, sonst gibt es kein internes Startsignal. Aufgrund des Bugs, sollten es nicht Bit0 bis Bit2 sein. Wenn auch kein Shadowregister benutzt werden soll, dann bleibt nur Bit3, was allerdings auch gleichzeitig eine Gearingfaktoränderung bis zum nächsten Startsignal hält. Daher folgendes Setup für das Startregister:
+         	bit3 = 1 (Startsignal notwendig für Gearingfaktoränderung)
+         	bit6 = 1 (Startsignal nach Target_reached-Event)
+         	bit12= 1 (pipeline on) 
+         	bit13= 1 (busy-state on)
+         	Rest=0
+         
+         */
+
+        /* my conclusions
+         
+         b 0 -> xtarget
+         b 3 -> bug
+         b 4 -> shaddow
+         b 6 -> target reach is start
+         b11 -> shaddow
+         b13 für busy (ab erstem oder erst mit nachfolgenden??
+         
+         start_out_add für 'wie lange warte ich '
+         
+         beim ersten eventuell b 5 für externen start??
+         */
+
+        //give all motors a nice start signal
+        if (!prepare_shaddow_registers) {
+          write43x(motors[i].cs_pin, START_CONFIG_REGISTER, 0
+            | _BV(0) //xtarget requires start
+          | _BV(1) //vmax requires start
+          | _BV(5) //external start is an start
+          | _BV(10)//immediate start
+          );   
+        } 
+        else {
+          write43x(motors[i].cs_pin, START_CONFIG_REGISTER, 0
+            | _BV(0) //from now on listen to your own start signal
+          | _BV(4)  //use shaddow motion profiles
+          | _BV(5)  //target reached triggers start event
+          | _BV(6)  //target reached triggers start event
+          | _BV(10) //immediate start
+          | _BV(11)  // the shaddow registers cycle
+          | _BV(13)  // coordinate yourself with busy starts
+          );   
+        }
+
       }
 
       //for the first move we need to configure everything a bit 
@@ -83,6 +147,9 @@ void checkMotion() {
 void target_reached_handler() {
   next_move_prepared=false;
 }
+
+
+
 
 
 
