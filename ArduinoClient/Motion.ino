3cdf57e4b9ed0997531a6634b226a24c300eb2d0
Interactive Matter <info@interactive-matter.org>
2013-12-02 17:15:15 +0100

    ok, setting the start signeals for the motors

--- ArduinoClient/Motion.ino
+++ ArduinoClient/Motion.ino
@@ -8,8 +8,8 @@ void startMotion() {
   prepare_shaddow_registers = false;
   //TODO initialize drivers??
   for (char i; i<nr_of_motors;i++) {
-        write43x(motors[i].cs_pin, START_OUT_ADD_REGISTER, 16000000ul);
-    }
+    write43x(motors[i].cs_pin, START_OUT_ADD_REGISTER, 16000000ul);
+  }
 }
 
 void stopMotion() {
@@ -59,66 +59,20 @@ void checkMotion() {
           moveMotor(i, 0,0, 0, prepare_shaddow_registers);
         }
 
-        /*
-  Master: (Startsignal generieren)
-         	bit4 = 1 (shadow on)
-         	bit9 je nach Bedarf und in Abstimmung mit dem Slave
-         	bit12= 1 (pipeline on)
-         
-         	rest = 0 (wenn kein zyklische und/oder verzögerte Shadowregisterübernahme gewünscht)
-         
-         Slave: (auf externes Startsignal reagieren)
-         	bit0 = 1
-         	bit5 = 1
-         	bit4 = 1 (shadow on)
-         	bit9 je nach Bedarf und in Abstimmung mit dem Master
-         	bit12= 1 (pipeline on)
-         
-         	rest = 0 (wenn kein zyklische und/oder verzögerte Shadowregisterübernahme gewünscht)
-         
-         Mindestens eines der ersten 5Bit muss eingeschaltet sein, sonst gibt es kein internes Startsignal. Aufgrund des Bugs, sollten es nicht Bit0 bis Bit2 sein. Wenn auch kein Shadowregister benutzt werden soll, dann bleibt nur Bit3, was allerdings auch gleichzeitig eine Gearingfaktoränderung bis zum nächsten Startsignal hält. Daher folgendes Setup für das Startregister:
-         	bit3 = 1 (Startsignal notwendig für Gearingfaktoränderung)
-         	bit6 = 1 (Startsignal nach Target_reached-Event)
-         	bit12= 1 (pipeline on) 
-         	bit13= 1 (busy-state on)
-         	Rest=0
-         
-         */
-
-        /* my conclusions
-         
-         b 0 -> xtarget
-         b 3 -> bug
-         b 4 -> shaddow
-         b 6 -> target reach is start
-         b11 -> shaddow
-         b13 für busy (ab erstem oder erst mit nachfolgenden??
-         
-         start_out_add für 'wie lange warte ich '
-         
-         beim ersten eventuell b 5 für externen start??
-         */
-
         //give all motors a nice start signal
         if (!prepare_shaddow_registers) {
           write43x(motors[i].cs_pin, START_CONFIG_REGISTER, 0
             | _BV(0) //xtarget requires start
           | _BV(1) //vmax requires start
           | _BV(5) //external start is an start
-          | _BV(6)  //target reached triggers start event
-          | _BV(10)//immediate start
+          | _BV(10)//immediate start         
           );   
         } 
         else {
           write43x(motors[i].cs_pin, START_CONFIG_REGISTER, 0
-       //     | _BV(0) //from now on listen to your own start signal
-         //   | _BV(3) //buggy?
+            | _BV(0) //x_target requires start
           | _BV(4)  //use shaddow motion profiles
-         // | _BV(5) //external start is an start
-          | _BV(6)  //target reached triggers start event
-          | _BV(10) //immediate start
-          | _BV(11)  // the shaddow registers cycle
-          | _BV(13)  // coordinate yourself with busy starts
+          | _BV(5) //external start is an start
           );   
         }
       }
@@ -171,6 +125,8 @@ void motor_target_reached(char motor_nr) {
   //and write down which motor touched the target
   motor_status |= _BV(motor_nr);  
 }
+
+
 
 
 
