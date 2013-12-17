61b41486c96a8bc58947aef81a791446f23dfefd
Interactive Matter <info@interactive-matter.org>
2013-12-17 12:58:56 +0100

    this is the official SPI …

--- EndstopTest/SPI.ino
+++ EndstopTest/SPI.ino
@@ -1,29 +1,24 @@
 unsigned char status;
 
 
-void write43x(unsigned char motor, unsigned char tmc43x_register, unsigned long datagram) {
-
-  send43x(motor, tmc43x_register | 0x80,datagram);
+void write43x(unsigned char cs_squirrel,unsigned char tmc43x_register, unsigned long datagram) {
+  send43x(cs_squirrel,tmc43x_register | 0x80,datagram);
 }
 
 unsigned long read43x(unsigned char cs_squirrel,unsigned char tmc43x_register, unsigned long datagram) {
-
   send43x(cs_squirrel,tmc43x_register, datagram);
-  unsigned long result =  send43x(cs_squirrel,tmc43x_register, datagram);
-
+  unsigned long result =  send43x(cs_squirrel, tmc43x_register, datagram);
   return result;
 }
 
-unsigned long send43x(unsigned char cs_squirrel,unsigned char tmc43x_register, unsigned long datagram) {
+long send43x(unsigned char cs_squirrel, unsigned char tmc43x_register, unsigned long datagram) {
   unsigned long i_datagram;
 
   //select the TMC driver
   digitalWrite(cs_squirrel,LOW);
 
 #ifdef DEBUG_SPI
-  Serial.print("To ");
-  Serial.print(tmc43x_register,HEX);
-  Serial.print(" sending ");
+  Serial.print("Sending ");
   Serial.println(datagram,HEX);
 #endif
 
@@ -40,7 +35,7 @@ unsigned long send43x(unsigned char cs_squirrel,unsigned char tmc43x_register, u
 
 #ifdef DEBUG_SPI
   Serial.print("Status :");
-  Serial.println(status,HEX);
+  Serial.println(status,BIN);
   Serial.print("Received ");
   Serial.println(i_datagram,HEX);
 #endif
@@ -51,14 +46,15 @@ unsigned long send43x(unsigned char cs_squirrel,unsigned char tmc43x_register, u
   return i_datagram;
 }
 
-void set260Register(unsigned char motor, unsigned long value) {
+
+void set260Register(unsigned char cs_pin, unsigned long value) {
   //santitize to 20 bits 
   value &= 0xFFFFF;
-  write43x(motor, COVER_LOW_REGISTER,value);  //Cover-Register: Einstellung des SMARTEN=aus
+  write43x(cs_pin, COVER_LOW_REGISTER,value);  //Cover-Register: Einstellung des SMARTEN=aus
 
-  read43x(motor, STATUS_REGISTER,0x0); //Abfrage Status, um SPI-Transfer zu beenden
-  read43x(motor, STATUS_REGISTER,0x0); //Abfrage Status, um SPI-Transfer zu beenden
-  read43x(motor, STATUS_REGISTER,0x0); //Abfrage Status, um SPI-Transfer zu beenden
+  read43x(cs_pin, STATUS_REGISTER,0x0); //Abfrage Status, um SPI-Transfer zu beenden
+  read43x(cs_pin, STATUS_REGISTER,0x0); //Abfrage Status, um SPI-Transfer zu beenden
+  read43x(cs_pin, STATUS_REGISTER,0x0); //Abfrage Status, um SPI-Transfer zu beenden
 }
 
 
