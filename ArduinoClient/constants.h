55f55f939497878d7907ca5e77cbca7eaf106c83
Interactive Matter <info@interactive-matter.org>
2013-12-19 13:19:14 +0100

    more comment more good

--- ArduinoClient/constants.h
+++ ArduinoClient/constants.h
@@ -53,5 +53,6 @@
 #define FIXED_8_24_MAKE(a)     (int32_t)((a*(1ul << 24ul)))
 #define FIXED_24_8_MAKE(a)     (int32_t)((a*(1ul << 8ul)))
 
+//how to mask REFERENCE_CONFIG_REGISTER if you want to configure just one end 
 #define LEFT_ENDSTOP_REGISTER_PATTERN (_BV(0) | _BV(2) | _BV(6) | _BV(10) | _BV(11) | _BV(14))
 #define RIGHT_ENDSTOP_REGISTER_PATTERN (_BV(1) | _BV(3) | _BV(7) | _BV(12) | _BV(13) | _BV(15))
