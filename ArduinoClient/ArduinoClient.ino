cc265211f8cea33b33fe7cfcd04ffeaf161029df
Interactive Matter <info@interactive-matter.org>
2014-01-09 16:36:21 +0100

    ok, we can reduce the logging

--- ArduinoClient/ArduinoClient.ino
+++ ArduinoClient/ArduinoClient.ino
@@ -12,12 +12,12 @@
 //# Debug Settings #
 //##################
 
-#define DEBUG_MOTOR_CONTFIG
-#define DEBUG_HOMING
-#define DEBUG_ENDSTOPS
+//#define DEBUG_MOTOR_CONTFIG
+//#define DEBUG_HOMING
+//#define DEBUG_ENDSTOPS
 
-//#define DEBUG_MOTION
-//#define DEBUG_MOTION_TRACE
+#define DEBUG_MOTION
+#define DEBUG_MOTION_TRACE
 //#define DEBUG_MOTOR_QUEUE
 
 //#define DEBUG_STATUS
--- gcode-printer/test/testprint.py
+++ gcode-printer/test/testprint.py
@@ -27,7 +27,7 @@ class Usage(Exception):
 def main(argv=None):
     try:
         #configure the overall logging
-        logging.basicConfig(filename='print.log', level=logging.DEBUG,
+        logging.basicConfig(filename='print.log', level=logging.INFO,
                             format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
         logging.info('Started')
 
