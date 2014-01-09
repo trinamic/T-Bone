8410bf4eac86388413755c2cd1486534e6bd057c
Interactive Matter <info@interactive-matter.org>
2014-01-09 16:06:00 +0100

    enough info …

--- ArduinoClient/ArduinoClient.ino
+++ ArduinoClient/ArduinoClient.ino
@@ -16,9 +16,9 @@
 #define DEBUG_HOMING
 #define DEBUG_ENDSTOPS
 
-#define DEBUG_MOTION
-#define DEBUG_MOTION_TRACE
-#define DEBUG_MOTOR_QUEUE
+//#define DEBUG_MOTION
+//#define DEBUG_MOTION_TRACE
+//#define DEBUG_MOTOR_QUEUE
 
 //#define DEBUG_STATUS
 
