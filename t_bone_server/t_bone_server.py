7a0ee8eec0c36ef1406622ece468be78d75f3af1
Interactive Matter <info@interactive-matter.org>
2014-01-27 11:20:00 +0100

    trying to reduce restart problems … did not work

--- t_bone_server/t_bone_server.py
+++ t_bone_server/t_bone_server.py
@@ -8,10 +8,11 @@ _logger = logging.getLogger(__name__)
 #this is THE printer - just a dictionary with anything
 _printer = None
 
+
 @app.route('/')
 def hello_world():
     template_dictionary = templating_defaults()
-    return render_template("index.html", ** template_dictionary)
+    return render_template("index.html", **template_dictionary)
 
 
 def templating_defaults():
@@ -21,11 +22,17 @@ def templating_defaults():
 
 
 if __name__ == '__main__':
-    _printer = helpers.create_printer()
-    config = json_config_file.read()
-    _printer.connect()
-    _printer.configure(config)
-    app.run(
-        host='0.0.0.0',
-        debug=True
-    )
+    #somehow we can get several initializations - hence we store a global printer
+    try:
+        if not _printer:
+            _printer = helpers.create_printer()
+            config = json_config_file.read()
+            _printer.connect()
+            _printer.configure(config)
+        app.run(
+            host='0.0.0.0',
+            debug=True
+        )
+    finally:
+        #reset the printer
+        _printer = None
