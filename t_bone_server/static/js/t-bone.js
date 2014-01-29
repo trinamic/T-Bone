e85add6a6cc1c7ec354009e394e24d8eac19c36a
Interactive Matter <info@interactive-matter.org>
2014-01-29 11:22:50 +0100

    hmm, this timer did not work …

--- t_bone_server/static/js/t-bone.js
+++ t_bone_server/static/js/t-bone.js
@@ -8,18 +8,16 @@ $(function () {
 })
 
 var last_status;
+function update_status() {
+    $.getJSON("/status", function (status_data) {
+        last_status = status_data;
+        $("body").trigger({
+            type: "status_update",
+            status_data: status_data
+        });
+    })
+}
 
 $().ready(function () {
-    $("body").timer(function () {
-            $.getJSON("/status", function (status_data) {
-                last_status = status_data
-                $("body").trigger({
-                    type: "status_update",
-                    status_data: status_data
-                });
-            })
-        },
-        delay = 1000,
-        repeat = true
-    )
+    setInterval("update_status()", 5000);
 })
