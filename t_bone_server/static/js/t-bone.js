0ff3282c56d6d837798396c6379beab9ffab0119
Interactive Matter <info@interactive-matter.org>
2014-01-29 10:27:03 +0100

    this should trigger the status update every second …

--- t_bone_server/static/js/t-bone.js
+++ t_bone_server/static/js/t-bone.js
@@ -1,8 +1,26 @@
-$( function() {
-    $(document).ajaxStart( function() {
-        $('.printer_function').prop('disabled',true);
+$(function () {
+    $(document).ajaxStart(function () {
+        $('.printer_function').prop('disabled', true);
     })
-    $(document).ajaxStop( function() {
-        $('.printer_function').prop('disabled',false);
+    $(document).ajaxStop(function () {
+        $('.printer_function').prop('disabled', false);
     })
+})
+
+var last_status;
+
+$().ready(function () {
+    $().timer(function () {
+            $.getJSON("/status", function (status_data) {
+                last_status = status_data
+                $("body").trigger({
+                    type: "status_update",
+                    status_data: status_data
+                });
+            })
+        },
+        delay = 1000,
+        repeat = true
+    )
+
 })
