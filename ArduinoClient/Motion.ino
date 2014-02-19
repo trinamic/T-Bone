77492eaafc3d7ad53be32ab6def5ac8fc5a05171
Interactive Matter <info@interactive-matter.org>
2014-02-19 12:27:01 +0100

    homing is implemented

--- ArduinoClient/Motion.ino
+++ ArduinoClient/Motion.ino
@@ -22,7 +22,7 @@ void startMotion(char initial_min_buffer_depth) {
     digitalWriteFast(motors[i].target_reached_interrupt_pin,LOW);
     attachInterrupt(motors[i].target_reached_interrupt_nr,motors[i].target_reached_interrupt_routine, FALLING);
     write43x(i, INTERRUPT_CONFIG_REGISTER, _BV(0) | _BV(1)); //POS_COMP_REACHED or TARGET_REACHED count as target reached
-    last_target[i]=0;//TODO isn't this more or less a homing 
+    last_target[i]=0;
   }
   next_move_prepared=false; //TODO in theory this is not needed  
   prepare_shaddow_registers = false;
