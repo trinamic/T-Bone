238577c23a965865918fe17dc8657d9e9c78fbfa
Interactive Matter <info@interactive-matter.org>
2014-03-13 14:18:19 +0100

    download the source code

--- install.sh
+++ install.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
+#install basic requirements
 opkg update
 opkg install python-pip python-setuptools python-smbus
+
+#download the tbone source
+curl -o t-bone.zip https://github.com/interactive-matter/trinamic3d/archive/master.zip
+unzip t-bone.zip
+
+#finally update the hostname
 echo t-bone > /etc/hostname
+echo "Hostname changed, it is a good idea to reboot, new hostname is:"
+cat /etc/hostname
