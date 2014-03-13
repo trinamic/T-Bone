b9c12232563670754df3165a898dc5858add0866
Interactive Matter <info@interactive-matter.org>
2014-03-13 12:41:43 +0100

    setup.py thankfully stolen from octoprint

---
+++ t_bone_server/setup.py
@@ -0,0 +1,57 @@
+# coding=utf-8
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+VERSION = "1.0.0-a1"
+
+
+def params():
+    name = "T Bone Server"
+    version = VERSION
+    description = "3D Printer for T Bone CNC cape"
+    long_description = open("README.md").read()
+    classifiers = [
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Framework :: Flask",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: Manufacturing",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: JavaScript",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Internet :: WWW/HTTP :: WSGI",
+        "Topic :: Printing",
+        "Topic :: System :: Networking :: Monitoring"
+    ]
+    author = "Marcus Nowotny"
+    author_email = "dev@t-bone.cc"
+    url = "http://tbone.cc"
+    license = "AGPLv3"
+
+    packages = find_packages(where="src")
+    package_dir = {"octoprint": "src/octoprint"}
+
+    include_package_data = True
+    zip_safe = False
+    install_requires = open("requirements.txt").read().split("\n")
+
+    #entry_points = {
+    #"console_scripts": [
+    #    "octoprint = octoprint:main"
+    #]
+    #}
+
+    #scripts = {
+    #	"scripts/octoprint.init": "/etc/init.d/octoprint"
+    #}
+
+    return locals()
+
+
+setup(**params())
