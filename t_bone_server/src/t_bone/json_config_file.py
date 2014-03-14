147a4c9eb4179b2be4e86b6129071fe8f6cfa6c3
Interactive Matter <info@interactive-matter.org>
2014-03-14 16:56:30 +0100

    ok the config is now really fix!

--- t_bone_server/src/t_bone/json_config_file.py
+++ t_bone_server/src/t_bone/json_config_file.py
@@ -2,8 +2,8 @@ import json
 
 __author__ = 'marcus'
 #TODO perhaps we shift it to some better place later - like /etc??
-_config_directory = '/home/root/t3d/server/'
-_config_file = _config_directory+'printer_config.json'
+_config_directory = '/etc/'
+_config_file = _config_directory+'tbone_config.json'
 
 
 def read():
