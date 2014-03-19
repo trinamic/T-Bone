2bbf29af3ea690d1535b69789428589112343475
Interactive Matter <info@interactive-matter.org>
2014-03-19 17:47:45 +0100

    hm the uart slot can be at different spots …

--- t_bone_server/src/t_bone/beaglebone_helpers.py
+++ t_bone_server/src/t_bone/beaglebone_helpers.py
@@ -8,7 +8,8 @@ _logger = logging.getLogger(__name__)
 
 _reset_pin = "P9_12"
 _default_serial_port = "/dev/ttyO1"
-_create_serial_port_script = "echo BB-UART1 > /sys/devices/bone_capemgr.9/slots"
+_serial_port_config_file = "/sys/devices/bone_capemgr.%s/slots"
+_create_serial_port_script = "echo BB-UART1 > %s"
 pwm_config = [
     {
         'temp': 'P9_39',
@@ -32,9 +33,14 @@ ALLOWED_EXTENSIONS = {'gcode'}
 
 
 def check_for_serial_port():
+    serial_port_config_file = "/sys/devices/bone_capemgr.9/slots"
+    for i in range(8, 9):
+        if os.path.exists(_serial_port_config_file % i):
+            serial_port_config_file = _serial_port_config_file % i
+            break
     if not os.path.exists(_default_serial_port):
         _logger.info("No serial port %s, creating it", _default_serial_port)
-        os.system(_create_serial_port_script)
+        os.system(_create_serial_port_script % serial_port_config_file)
     return _default_serial_port
 
 
