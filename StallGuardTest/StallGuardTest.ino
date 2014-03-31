7f309a3100181d0668e4399dcde3c774fc32e905
Interactive Matter <info@interactive-matter.org>
2014-03-31 14:13:27 +0200

    starting the stallguard test
    … with something we already have …

---
+++ StallGuardTest/StallGuardTest.ino
@@ -0,0 +1,25 @@
+#include <digitalIOPerformance.h>
+#include <SPI.h>
+#include "constants.h"
+
+#define CS_4361_1_PIN 4
+#define CS_4361_2_PIN 12
+#define CS_4361_3_PIN 6
+#define START_SIGNAL_PIN A3
+#define CS_5041_PIN 11
+
+#define INT_5041_PIN 8
+#define INT_4361_1_PIN 3
+#define INT_4361_2_PIN 2
+#define INT_4361_3_PIN 7
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly: 
+  
+}
