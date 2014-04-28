aeb143e65259f10ac6011d6375c64d7bd970cc07
Interactive Matter <info@interactive-matter.org>
2014-04-28 10:43:23 +0200

    disabling the X-server

--- install.sh
+++ install.sh
@@ -7,6 +7,9 @@ opkg install python-pip python-setuptools python-smbus
 curl -o t-bone.zip https://github.com/interactive-matter/trinamic3d/archive/master.zip
 unzip t-bone.zip
 
+#disabling x according to http://curiouslynerdy.com/2013/09/disable-gui-on-the-beaglebone-black/
+systemctl enable multi-user.target
+
 #finally update the hostname
 echo t-bone > /etc/hostname
 echo "Hostname changed, it is a good idea to reboot, new hostname is:"
