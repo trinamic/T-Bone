1d5b8dda35d7fcb13b270bc9abbd0bf7bd9fc56c
Interactive Matter <info@interactive-matter.org>
2014-04-28 11:03:15 +0200

    disabling more services

--- install.sh
+++ install.sh
@@ -9,7 +9,14 @@ unzip t-bone.zip
 
 #disabling x according to http://curiouslynerdy.com/2013/09/disable-gui-on-the-beaglebone-black/
 systemctl enable multi-user.target
-
+#tailoring more services according to http://www.element14.com/community/community/knode/single-board_computers/blog/2014/02/27/beaglebone-black-radio-challenge--part-3-user-interface
+systemctl disable bonescript.service  
+systemctl disable bonescript-autorun.service
+rm '/etc/systemd/system/multi-user.target.wants/bonescript-autorun.service'  
+systemctl disable cloud9.service  
+rm '/etc/systemd/system/multi-user.target.wants/cloud9.service'  
+systemctl disable bonescript.socket  
+rm '/etc/systemd/system/sockets.target.wants/bonescript.socket'
 #finally update the hostname
 echo t-bone > /etc/hostname
 echo "Hostname changed, it is a good idea to reboot, new hostname is:"
